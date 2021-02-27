7f871659fa6effef0d0e83211c5da866cfbc8e8b
jeffgrock <jeff.grockowski@protonmail.com>
2021-02-27 17:44:01 -0500

    first commit

---
+++ B&B_website/index.php
@@ -0,0 +1,29 @@
+<?php include 'views/header.php'?>
+
+<body>
+<main>
+    <div>
+    <table>
+        <tr>
+            <td>home page</td>
+        </tr>
+        <tr>
+            <td>beer list</td>
+        </tr>
+        <tr>
+            <td>calendar</td>
+        </tr>
+        <tr>
+            <td>bakery</td>
+        </tr>
+        <td>
+            <td>cart</td>
+        </td>
+
+
+
+    </table>
+    </div>
+</main>
+
+<?php include 'views/footer.php'; ?>
---
+++ B&B_website/views/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head> 
+
+    <title>My Guitar Shop</title>
+    <meta>
+    <link rel="stylesheet" type="scss/main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1 id = 'main_logo'>B & B</h1></header>
